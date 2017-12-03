3951b4fed16efe1b150f8fce36e1fad0a7bcef8a
novar <riccardo.novaglia@gmail.com>
2017-12-03 12:18:21 +0000

    Improve argument parsing and output.

--- master_review.sh
+++ master_review.sh
@@ -1,33 +1,42 @@
 #!/usr/bin/env bash
 
-[[ "$#" -eq 1 ]] || { echo "Please provide a name for your Conversation" >&2; exit 1; }
-CONVERSATION_NAME=`echo $1 | tr " " _`
+CONVERSATION_NAME=""
+START_COMMIT=""
+END_COMMIT=""
 
-START_COMMIT=`git rev-parse HEAD~2`
-END_COMMIT=`git rev-parse HEAD`
+while getopts 'c:s:e:' flag; do
+  case "${flag}" in
+    c) CONVERSATION_NAME="${OPTARG}" ;;
+    s) START_COMMIT="${OPTARG}" ;;
+    e) END_COMMIT="${OPTARG}" ;;
+    *) error "Unexpected option ${flag}" ;;
+  esac
+done
 
-function createBranch {
-    git checkout -b ${1}
-    git push --set-upstream origin ${1}
-}
+[[ ! -z ${CONVERSATION_NAME} ]] || { echo "Please provide a name for your Conversation" >&2; exit 1; }
+[[ ! -z ${START_COMMIT} ]] || { echo "Please provide a start commit (can be symbolic)" >&2; exit 1; }
+[[ ! -z ${END_COMMIT} ]] || { echo "Please provide an commit (can be symbolic)" >&2; exit 1; }
 
-function setRepoURL {
-    REMOTE=`git remote -v | grep push | awk '{split($0, a, " "); print a[2]}'`
-    REPO_URL=`echo ${REMOTE} | rev | cut -c5- | rev`
-}
+VALIDATED_CONVERSATION_NAME=`echo ${CONVERSATION_NAME} | tr " " _`
+START_COMMIT_HASH=`git rev-parse ${START_COMMIT}`
+END_COMMIT_HASH=`git rev-parse ${END_COMMIT}`
 
+echo "Setting up conversation [$VALIDATED_CONVERSATION_NAME] between commits [$START_COMMIT_HASH] and [$END_COMMIT_HASH]"
 
-git checkout ${START_COMMIT}
-START_BRANCH_NAME="${CONVERSATION_NAME}_begin"
+function createBranch { git checkout -b ${1}; git push -q --set-upstream origin ${1}; }
+function quietlyCheckout { git checkout -q ${1}; }
+
+quietlyCheckout ${START_COMMIT_HASH}
+START_BRANCH_NAME="${VALIDATED_CONVERSATION_NAME}_begin"
 createBranch ${START_BRANCH_NAME}
 
-git checkout ${END_COMMIT}
-END_BRANCH_NAME="${CONVERSATION_NAME}"
+quietlyCheckout ${END_COMMIT_HASH}
+END_BRANCH_NAME="${VALIDATED_CONVERSATION_NAME}"
 createBranch ${END_BRANCH_NAME}
 
-setRepoURL
+REPO_URL=`git remote -v | grep push | awk '{split($0, a, " "); print a[2]}' | rev | cut -c5- | rev`
 open "${REPO_URL}/compare/${START_BRANCH_NAME}...${END_BRANCH_NAME}"
 
-git checkout master
+git checkout -q master
 git branch -D ${START_BRANCH_NAME}
 git branch -D ${END_BRANCH_NAME}
