5d532cc0c03f65eef5c508c9a2d020a7fe53a452
novar <riccardo.novaglia@gmail.com>
2017-12-03 11:30:11 +0000

    Accept Conversation name as first argument.

--- master_review.sh
+++ master_review.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-FEATURE_NAME="A_NEW_FEATURE"
+[[ "$#" -eq 1 ]] || { echo "Please provide a name for your Conversation" >&2; exit 1; }
+FEATURE_NAME=`echo $1 | tr " " _`
 
 FEATURE_START_COMMIT=`git rev-parse HEAD~2`
 FEATURE_END_COMMIT=`git rev-parse HEAD`
@@ -28,3 +29,5 @@ setRepoURL
 open "${REPO_URL}/compare/${FEATURE_START_BRANCH_NAME}...${FEATURE_END_BRANCH_NAME}"
 
 git checkout master
+git branch -D ${FEATURE_START_BRANCH_NAME}
+git branch -D ${FEATURE_END_BRANCH_NAME}
