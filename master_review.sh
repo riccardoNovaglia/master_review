dcc787cc4e466864d89726ecc55e31842d965b33
novar <riccardo.novaglia@gmail.com>
2017-12-03 11:33:23 +0000

    Remove the word feature from the script. This is not necessarily only useful for complete features.

--- master_review.sh
+++ master_review.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 [[ "$#" -eq 1 ]] || { echo "Please provide a name for your Conversation" >&2; exit 1; }
-FEATURE_NAME=`echo $1 | tr " " _`
+CONVERSATION_NAME=`echo $1 | tr " " _`
 
-FEATURE_START_COMMIT=`git rev-parse HEAD~2`
-FEATURE_END_COMMIT=`git rev-parse HEAD`
+START_COMMIT=`git rev-parse HEAD~2`
+END_COMMIT=`git rev-parse HEAD`
 
 function createBranch {
     git checkout -b ${1}
@@ -17,17 +17,17 @@ function setRepoURL {
 }
 
 
-git checkout ${FEATURE_START_COMMIT}
-FEATURE_START_BRANCH_NAME="${FEATURE_NAME}_begin"
-createBranch ${FEATURE_START_BRANCH_NAME}
+git checkout ${START_COMMIT}
+START_BRANCH_NAME="${CONVERSATION_NAME}_begin"
+createBranch ${START_BRANCH_NAME}
 
-git checkout ${FEATURE_END_COMMIT}
-FEATURE_END_BRANCH_NAME="${FEATURE_NAME}"
-createBranch ${FEATURE_END_BRANCH_NAME}
+git checkout ${END_COMMIT}
+END_BRANCH_NAME="${CONVERSATION_NAME}"
+createBranch ${END_BRANCH_NAME}
 
 setRepoURL
-open "${REPO_URL}/compare/${FEATURE_START_BRANCH_NAME}...${FEATURE_END_BRANCH_NAME}"
+open "${REPO_URL}/compare/${START_BRANCH_NAME}...${END_BRANCH_NAME}"
 
 git checkout master
-git branch -D ${FEATURE_START_BRANCH_NAME}
-git branch -D ${FEATURE_END_BRANCH_NAME}
+git branch -D ${START_BRANCH_NAME}
+git branch -D ${END_BRANCH_NAME}
